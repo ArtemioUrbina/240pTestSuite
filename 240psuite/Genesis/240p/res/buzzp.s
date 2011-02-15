34f2e14ed58b9639d0787bed0b657d9f2b3e4b19
Artemio Urbina <ArtemioUrbina@users.noreply.github.com>
2011-02-15 21:12:13 +0000

    - Added new background and sprite with drop shadow to the Shadow test

---
+++ 240psuite/Genesis/240p/res/buzzp.s
@@ -0,0 +1,24 @@
+* ---------------------------
+.global buzz_pal
+buzz_pal:
+* ---------------------------
+* size:32 bytes
+
+
+* ---------------------------
+	dc.w 0x0f0f
+	dc.w 0x0eee
+	dc.w 0x0e88
+	dc.w 0x0c66
+	dc.w 0x0aaa
+	dc.w 0x0a44
+	dc.w 0x0888
+	dc.w 0x0822
+	dc.w 0x0444
+	dc.w 0x00ee
+	dc.w 0x0008
+	dc.w 0x0004
+	dc.w 0x0000
+	dc.w 0x0000
+	dc.w 0x0000
+	dc.w 0x0000
---
+++ 240psuite/Genesis/240p/res/sonicbackp.s
@@ -0,0 +1,24 @@
+* ---------------------------
+.global sonicback_pal
+sonicback_pal:
+* ---------------------------
+* size:32 bytes
+
+
+* ---------------------------
+	dc.w 0x0000
+	dc.w 0x0fdb
+	dc.w 0x0f96
+	dc.w 0x0b96
+	dc.w 0x0026
+	dc.w 0x0d92
+	dc.w 0x0b02
+	dc.w 0x00f9
+	dc.w 0x00df
+	dc.w 0x00b4
+	dc.w 0x009f
+	dc.w 0x006d
+	dc.w 0x0049
+	dc.w 0x0fb9
+	dc.w 0x0060
+	dc.w 0x0040
