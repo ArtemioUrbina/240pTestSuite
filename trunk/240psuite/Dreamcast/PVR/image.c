745d89d0c51f00ac1735cd8e1b0853cae10ef2c6
junkerhq@gmail.com <junkerhq@gmail.com@aba5e28d-7ef9-402c-a2d1-502d70bd50dc>
2011-02-16 01:05:37 +0000

    - fixed dumb mistakes in licence.

--- trunk/240psuite/Dreamcast/PVR/image.c
+++ trunk/240psuite/Dreamcast/PVR/image.c
@@ -4,7 +4,7 @@
  *
  * This file is part of the 240p Test Suite
  *
- * To Eleven is free software; you can redistribute it and/or modify
+ * The 240p Test Suite is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
