5b1ae952b0b8ca15cc3d7fd9effd3095e6871ffd
junkerhq@gmail.com <junkerhq@gmail.com@aba5e28d-7ef9-402c-a2d1-502d70bd50dc>
2011-02-18 23:30:20 +0000

    - Cleaned up and separated Genesis code in several files

---
+++ trunk/240psuite/Genesis/240p/patterns.h
@@ -0,0 +1,12 @@
+#ifndef PATTERNS_H
+#define PATTERNS_H
+
+void DrawPlunge();
+void DrawColorBars();
+void Draw601ColorBars();
+void DrawGrid();
+void DrawLinearity();
+void DrawGrayRamp();
+void DrawWhiteScreen();
+
+#endif
