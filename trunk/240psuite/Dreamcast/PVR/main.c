716ca2321a14061552ba27aacf1189ec071335b1
junkerhq@gmail.com <junkerhq@gmail.com@aba5e28d-7ef9-402c-a2d1-502d70bd50dc>
2011-02-19 06:08:05 +0000

    - Release 1.4

--- trunk/240psuite/Dreamcast/PVR/main.c
+++ trunk/240psuite/Dreamcast/PVR/main.c
@@ -1639,7 +1639,7 @@ void DrawCredits()
 		DrawStringS(x, y, 0.0, 1.0, 0.0, "Info on using this suite:"); y += fh; 
 		DrawStringS(x+5, y, 1.0, 1.0, 1.0, "http://junkerhq.net/xrgb/"); y += fh; 
 
-		DrawStringS(220, 58, 1.0, 1.0, 1.0, "Ver. 1.3"); y += fh; 
+		DrawStringS(220, 58, 1.0, 1.0, 1.0, "Ver. 1.4"); y += fh; 
 
 		DrawScanlines();
 		pvr_list_finish();        
