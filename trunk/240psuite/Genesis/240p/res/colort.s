6a61daa8ffffa0ac74aa1d8a1a5012f1f4165068
junkerhq@gmail.com <junkerhq@gmail.com@aba5e28d-7ef9-402c-a2d1-502d70bd50dc>
2011-02-24 23:36:52 +0000

    - Colorbars now use Shadow/Highlights mode in order to display more colors simultaneously, this will also improve the PLUGE considerably. Just need to draw it tile based by hand now.

---
+++ trunk/240psuite/Genesis/240p/res/colort.s
@@ -0,0 +1,77 @@
+* ---------------------------
+.global color_tiles
+color_tiles:
+* ---------------------------
+* size:256 bytes
+
+
+* ---------------------------
+	dc.l 0xeeeeeeee
+	dc.l 0xeeeeeeee
+	dc.l 0xeeeeeeee
+	dc.l 0xeeeeeeee
+	dc.l 0xeeeeeeee
+	dc.l 0xeeeeeeee
+	dc.l 0xeeeeeeee
+	dc.l 0xeeeeeeee
+
+* ---------------------------
+	dc.l 0xcccccccc
+	dc.l 0xcccccccc
+	dc.l 0xcccccccc
+	dc.l 0xcccccccc
+	dc.l 0xcccccccc
+	dc.l 0xcccccccc
+	dc.l 0xcccccccc
+	dc.l 0xcccccccc
+
+* ---------------------------
+	dc.l 0xaaaaaaaa
+	dc.l 0xaaaaaaaa
+	dc.l 0xaaaaaaaa
+	dc.l 0xaaaaaaaa
+	dc.l 0xaaaaaaaa
+	dc.l 0xaaaaaaaa
+	dc.l 0xaaaaaaaa
+	dc.l 0xaaaaaaaa
+
+* ---------------------------
+	dc.l 0x88888888
+	dc.l 0x88888888
+	dc.l 0x88888888
+	dc.l 0x88888888
+	dc.l 0x88888888
+	dc.l 0x88888888
+	dc.l 0x88888888
+	dc.l 0x88888888
+
+* ---------------------------
+	dc.l 0x66666666
+	dc.l 0x66666666
+	dc.l 0x66666666
+	dc.l 0x66666666
+	dc.l 0x66666666
+	dc.l 0x66666666
+	dc.l 0x66666666
+	dc.l 0x66666666
+
+* ---------------------------
+	dc.l 0x44444444
+	dc.l 0x44444444
+	dc.l 0x44444444
+	dc.l 0x44444444
+	dc.l 0x44444444
+	dc.l 0x44444444
+	dc.l 0x44444444
+	dc.l 0x44444444
+
+* ---------------------------
+	dc.l 0x22222222
+	dc.l 0x22222222
+	dc.l 0x22222222
+	dc.l 0x22222222
+	dc.l 0x22222222
+	dc.l 0x22222222
+	dc.l 0x22222222
+	dc.l 0x22222222
+
