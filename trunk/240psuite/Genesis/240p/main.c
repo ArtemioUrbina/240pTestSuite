5d5a3ddfc072a8b795001901f94b4d126125929b
junkerhq@gmail.com <junkerhq@gmail.com@aba5e28d-7ef9-402c-a2d1-502d70bd50dc>
2012-01-25 18:46:03 +0000

    - Added new black/white color scheme for Lag Test

--- trunk/240psuite/Genesis/240p/main.c
+++ trunk/240psuite/Genesis/240p/main.c
@@ -309,7 +309,7 @@ void DrawCredits()
   VDP_drawTextBG(APLAN, "Info on using this test suite:", TILE_ATTR(PAL1, 0, 0, 0), 4, pos++);
   VDP_drawTextBG(APLAN, "http://junkerhq.net/xrgb", TILE_ATTR(PAL0, 0, 0, 0), 5, pos++);
 
-  VDP_drawTextBG(APLAN, "Ver. 1.10c", TILE_ATTR(PAL0, 0, 0, 0), 26, 6);
+  VDP_drawTextBG(APLAN, "Ver. 1.10e", TILE_ATTR(PAL0, 0, 0, 0), 26, 6);
   while(!exit)
   {
     buttons = JOY_readJoypad(JOY_1);
