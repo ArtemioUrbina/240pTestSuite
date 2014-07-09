c192d478b2f88e76c4864accca03d50e0dd93833
artemiourbina <artemiourbina@aba5e28d-7ef9-402c-a2d1-502d70bd50dc>
2014-07-09 07:12:01 +0000

    - Skeleton for a basic SNES version, only has grid for now

---
+++ trunk/240psuite/SNES/240p/data.asm
@@ -0,0 +1,47 @@
+.include "hdr.asm"
+
+.section ".rodata1" superfree
+
+back_tiles:
+.incbin "back.pic"
+back_tiles_end:
+
+gillian_tiles:
+.incbin "gillian.pic"
+gillian_tiles_end:
+
+grid_tiles:
+.incbin "grid.pic"
+grid_tiles_end:
+
+.ends
+
+.section ".rodata2" superfree
+back_map:
+.incbin "back.map"
+back_map_end:
+
+back_pal:
+.incbin "back.pal"
+back_pal_end:
+
+grid_map:
+.incbin "grid.map"
+grid_map_end:
+
+grid_pal:
+.incbin "grid.pal"
+grid_pal_end:
+
+gillian_pal:
+.incbin "gillian.pal"
+gillian_pal_end:
+
+.ends
+
+.section ".rodata3" superfree
+
+font:
+.incbin "font.pic"
+
+.ends
