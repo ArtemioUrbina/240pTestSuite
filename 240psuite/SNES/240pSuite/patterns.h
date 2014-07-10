67441a1c5d92012f65d6873f74de75da9949b0fc
Artemio Urbina <ArtemioUrbina@users.noreply.github.com>
2014-07-10 15:31:06 +0000

    - Updated structure and licence

--- 240psuite/SNES/240pSuite/patterns.h
+++ 240psuite/SNES/240pSuite/patterns.h
@@ -19,18 +19,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
  
-extern char back_tiles, back_tiles_end;
-extern char back_pal, back_pal_end;
-extern char back_map, back_map_end;
+#include "res.h"
+#include "video.h"
 
-extern char gillian_tiles, gillian_tiles_end;
-extern char gillian_pal, gillian_pal_end;
-extern char gillian_map, gillian_map_end;
-
-extern char grid_tiles, grid_tiles_end;
-extern char grid_pal, grid_pal_end;
-extern char grid_map, grid_map_end;
-
-extern char grid240_map, grid240_map_end;
-
-extern char font;
+//---------------------------------------------------------------------------------
+void DrawGrid();
--- 240psuite/SNES/240pSuite/video.c
+++ 240psuite/SNES/240pSuite/video.c
@@ -19,18 +19,16 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
  
-extern char back_tiles, back_tiles_end;
-extern char back_pal, back_pal_end;
-extern char back_map, back_map_end;
+#include "video.h"
 
-extern char gillian_tiles, gillian_tiles_end;
-extern char gillian_pal, gillian_pal_end;
-extern char gillian_map, gillian_map_end;
+#define REG_SETINI (*(vuint8*)0x2133)
 
-extern char grid_tiles, grid_tiles_end;
-extern char grid_pal, grid_pal_end;
-extern char grid_map, grid_map_end;
+void Set240pMode()
+{
+	REG_SETINI |= 0x04;
+}
 
-extern char grid240_map, grid240_map_end;
-
-extern char font;
+void Set224pMode()
+{
+	REG_SETINI = 0;
+}
