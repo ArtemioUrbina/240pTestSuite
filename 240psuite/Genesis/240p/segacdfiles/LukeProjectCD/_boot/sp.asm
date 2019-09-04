70a89a2f782c003be1f32f4c32a0a47b410dfab1
Artemio Urbina <aurbina@junkerhq.net>
2019-09-04 01:44:48 -0500

    - Updated CDDA commands - Cleaned makefile -- It doesn't align the binary, so that the Load bug when reaching 256Kb doesn't occur

--- 240psuite/Genesis/240p/segacdfiles/LukeProjectCD/_boot/sp.asm
+++ 240psuite/Genesis/240p/segacdfiles/LukeProjectCD/_boot/sp.asm
@@ -67,7 +67,7 @@ SP_Init:
 DriveInit_Params:
 		dc.b	1, $FF			; first track, last track (all)
 		even				    ; just in case
-CDDA_TrkBuf:
+Buffer:
 		dc.l	2
 ; =======================================================================================
 ;  SP_Main
@@ -101,9 +101,11 @@ OpTable:
 		bra.w	Op_LoadBootFile		; Load File (from ISO9660 filesystem)
 		bra.w	Op_GetWordRAM		; Give WordRAM to Main CPU
         bra.w	Op_InitCD		    ; Init
-        bra.w	Op_PlayCDMDF	    ; Play CD-DA Track 2 for MDFourier
-		bra.w	Op_PlayCD240	    ; Play CD-DA Track 3 for the Suite
+		bra.w	Op_SeekCDMDF	    ; Seek CD-DA Track 2 for MDFourier
+		bra.w	Op_UnPauseCD	    ; Play (unpause) CD-DA Track
         bra.w	Op_StopCD		    ; Stop CD-DA Track
+		bra.w	Op_PlayCDMDF	    ; Play CD-DA Track 2 for the MDFourier Debug
+		bra.w	Op_PlayCD240	    ; Play CD-DA Track 3 for the Suite
 		bra.w	Op_PlayPCM			; Play Full PCM Memory
 		bra.w	Op_StopPCM			; Stop PCM Playback
 		bra.w	Op_SetPCMLeft		; Set PCM for Left Channel
@@ -145,44 +147,41 @@ Op_InitCD:
 		BIOS_CDCSTOP
 		BIOS_CDCSTAT
         rts
-
-; taken form the sonic 1 on sega cd project 
-; https://info.sonicretro.org/Sonic_the_Hedgehog_1_for_SegaCD		
-
+	
 Op_PlayCDMDF:
-		moveq	#0,d1
-		move.w	$ffff8010,d1
-		addq.b	#2,d1
-		BIOS_MSCSTOP
-		BIOS_CDBTOCREAD
-		BIOS_MSCSTOP			; Just in case
-		move.w	$ffff8010,d1
-		addq.b	#2,d1
-		andi.w	#$FF,d1			; Just in case
-		lea	(CDDA_TrkBuf).l,a0
-		move.w	d1,(a0)
-		BIOS_MSCPLAY1 
-		rts
+		move.w	#2,d1
+		jmp PlayCDDA
 		
 Op_PlayCD240:
-		moveq	#0,d1
-		move.w	$ffff8010,d1
-		addq.b	#3,d1
-		BIOS_MSCSTOP
-		BIOS_CDBTOCREAD
-		BIOS_MSCSTOP			; Just in case
-		move.w	$ffff8010,d1
-		addq.b	#3,d1
-		andi.w	#$FF,d1			; Just in case
-		lea	(CDDA_TrkBuf).l,a0
+		move.w	#3,d1
+		jmp PlayCDDA
+		
+PlayCDDA:
+		lea		(Buffer).l,a0
 		move.w	d1,(a0)
-		BIOS_MSCPLAY1 
+		BIOS_MSCPLAY1
 		rts
 		
+Op_UnPauseCD:
+		BIOS_MSCPAUSEOFF
+		rts
+		
+Op_SeekCDMDF:
+        move.w	#$ffff,d1    			;Removes CD Pause timeout, reset at stop
+        BIOS_CDBPAUSE
+
+		move.w	#2,d1
+		lea		(Buffer).l,a0
+		move.w	d1,(a0)
+		BIOS_MSCSEEK1
+		rts
+
 Op_StopCD:
 		BIOS_MSCSTOP
 
- 		rts
+        move.w	#$1194,d1    			;Sets the CD Pause timeout to the minimum value after stop
+        BIOS_CDBPAUSE
+		rts
 
 Op_PlayPCM:
 		move.b	#$FF, ENVdat			; Set ENV to FF - full volume
@@ -193,7 +192,6 @@ Op_PlayPCM:
 
 		move.b	#$FE, ONOFFdat			; Set channel 1 to on
 	    bsr		PCMWait
-
 		rts
 		
 Op_StopPCM:			
@@ -208,7 +206,6 @@ Op_StopPCM:
     
 	    move.b  #$00, FDLdat			; Set L byte of sample rate
 	    bsr		PCMWait
-		
 		rts
 
 PCMTestFreq:
@@ -262,7 +259,6 @@ InitPCM:
     
 	    move.b  #$00, FDLdat			; Set L byte of sample rate
 	    bsr		PCMWait
-    
 	    rts
 		
 Op_SetPCMLeft:
@@ -282,14 +278,11 @@ Op_SetPCMCenter:
 
 PCMWait:
 	    move.l	d0, -(a7)
-    
 	    move.w  #PCMREGDELAY, d0
 
 PCMWaitLoop:
 	    dbra	d0, PCMWaitLoop
-    
 	    move.l	(a7)+, d0
-    
 	    rts
 
 pcmsweepfile:
@@ -333,7 +326,7 @@ pcmtest:
 		align 2
 		
 Op_SetSamplesTest:
-		lea		pcmtest(pc),a0	; Sweep 1-16khz
+		lea		pcmtest(pc),a0	; Never Give Up!
 		bsr		LoadPCMFile
 		rts
 		
@@ -342,17 +335,18 @@ pcmtest2:
 		align 2
 		
 Op_SetSamplesTest2:
-		lea		pcmtest2(pc),a0	; Sweep 1-16khz
+		lea		pcmtest2(pc),a0	; Fourier Art
 		bsr		LoadPCMFile
 		rts
 
 LoadPCMFile:
 		bsr		FindFile		; Find File returns params in the right format for ReadCD
-		move.l	#$20000, a0		; Set destination to ProgramRAM
+		move.l	#$20000, a0		; Set destination to Program RAM
 		bsr		ReadCD
 				
 ; d1.l has the size from above in sectors (2048)
-; but this code is (still) hard coded for 64kb
+; but this code is (still) hardcoded for 64kb
+
 		move.l	#$20000, a2		; A2 - PCM Address
 		move.b	#0,d2			; D2 - start at first bank
 LoadPCMLoop:		
@@ -364,8 +358,7 @@ LoadPCMLoop:
 		add.l	#$1000,a2		; Increment address by 1000h
 		cmp.w   #$10,d2
 		BNE     LoadPCMLoop
-		; our sample already has the FF terminators
-		
+		; our samples already have the FF terminators for loop
 		rts
 
 
@@ -373,13 +366,13 @@ LoadPCMLoop:
 ; D1 - Bank
 ; A0 - PCM Address
 WritePCM:
-		or.b	#$80, d1				; 1000b - bit 4 = 1 = play on bit 3 = 0 = select bank
-		move.b	d1, CTRLdat				; Select bank to write into
+		or.b	#$80, d1		; 1000b - bit 4 = 1 = play on bit 3 = 0 = select bank
+		move.b	d1, CTRLdat		; Select bank to write into
 		bsr		PCMWait
 
-		move.l	#WAVEdat, a1			; Get pointer to start of PCM bank
+		move.l	#WAVEdat, a1	; Get pointer to start of PCM bank
 
-		sub.l	#1, d0					; Adjust for dbra
+		sub.l	#1, d0			; Adjust for dbra
 
 @WritePCMLoop:
 		move.b	(a0)+, (a1)
@@ -412,22 +405,22 @@ ReadCD:
 		BIOS_ROMREADN			; Begin data read
 @waitSTAT:
 		BIOS_CDCSTAT			; Check CDC status
-		bcs	@waitSTAT		; If not ready, branch
+		bcs	@waitSTAT			; If not ready, branch
 @waitREAD:
 		BIOS_CDCREAD			; Read data
-		bcc	@waitREAD		; If not done, branch
+		bcc	@waitREAD			; If not done, branch
 @WaitTransfer:
 		movea.l	8(a5), a0		; Get destination address
-		lea	12(a5), a1		; Get header
-		BIOS_CDCTRN			; Transfer sector
+		lea	12(a5), a1			; Get header
+		BIOS_CDCTRN				; Transfer sector
 		bcc	@waitTransfer		; If not done, branch
-		BIOS_CDCACK			; Acknowledge transfer
+		BIOS_CDCACK				; Acknowledge transfer
 		addq.l	#1, (a5)		; Increment starting sector
 		addi.l	#$0800, 8(a5)	; Increment destination address
 		subq.l	#1, 4(a5)		; Decrement sectors left
-		bne	@waitSTAT		; If not finished, branch
-		pop	d0-d7/a0-a6		; Restore all registers
-		rts				; Return
+		bne	@waitSTAT			; If not finished, branch
+		pop	d0-d7/a0-a6			; Restore all registers
+		rts						; Return
 
 		
 ; =======================================================================================
@@ -446,16 +439,16 @@ Header:
 Init9660:
 		push	d0-d7/a0-a6		; Store all registers
 
-						; Load Volume VolumeDescriptor
+		; Load Volume VolumeDescriptor
 
 		move.l	#$10, d0		; Start Sector
 		move.l  #2, d1			; Size in sector
 		lea.l	SectorBuffer, a0	; Destination
-		bsr	ReadCD			; Read Data
+		bsr	ReadCD				; Read Data
 
-						; Load Root Directory
+		; Load Root Directory
 
-		lea.l	SectorBuffer,a0		; Get pointer to sector buffer
+		lea.l	SectorBuffer,a0	; Get pointer to sector buffer
 		lea.l	156(a0),a1		; Get root directory record
 		
 		move.b	6(a1),d0		; Get first part of Sector address
@@ -481,7 +474,7 @@ Init9660:
 ; =======================================================================================
 FindFile:
 		push	a1/a2/a6		; Store used registers
-		lea.l	SectorBuffer,a1		; Get sector buffer
+		lea.l	SectorBuffer,a1	; Get sector buffer
 @ReadFilenameStart:
 		movea.l	a0,a6			; Store filename pointer
 		move.b	(a6)+,d0		; Read character from filename
@@ -491,7 +484,7 @@ FindFile:
 		bne.b	@findFirstChar		; If not matched, branch
 @checkChars:
 		move.b	(a6)+,d0		; Read next charactor of filename and increment
-		beq	@getInfo		; If all characters were matched, branch			
+		beq		@getInfo		; If all characters were matched, branch			
 		cmp.b	(a1)+,d0		; Else, check next character
 		bne.b	@ReadFilenameStart	; If not matched, find next file
 		bra.b	@checkChars		; else, check next character
@@ -505,7 +498,7 @@ FindFile:
 		move.b	8(a2),d0		; get next part of sector address
 		lsl.l	#8,d0			; bitshift
 		move.b	9(a2),d0		; get final part of sector address.
-						; d0 now contains start sector address
+								; d0 now contains start sector address
 
 		move.b	14(a2),d1		; Same as above, but for FileSize
 		lsl.l	#8,d1
@@ -518,7 +511,7 @@ FindFile:
 		lsr.l	#8,d1			; Bitshift filesize (to get sector count)
 		lsr.l	#3,d1
 	
-		pop	a1/a2/a6		; Restore used registers	
+		pop	a1/a2/a6			; Restore used registers	
 		rts
 
 ; =======================================================================================
