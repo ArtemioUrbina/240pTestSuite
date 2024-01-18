16456ff4fd89ffa7eea634c1ca88625da85101e3
hitomi2500 <hitomi2500@mail.ru>
2024-01-18 19:14:25 +0300

    Saturn boot logo

---
+++ 240psuite/Saturn/240pTestSuite/image_buzzbomber.h
@@ -0,0 +1,7 @@
+#ifndef IMAGE_BUZZBOMBER_H
+#define IMAGE_BUZZBOMBER_H
+
+extern unsigned char image_buzzbomber_cmap[256][3];
+extern unsigned char image_buzzbomber_data[32*32];
+
+#endif /* !IMAGE_BUZZBOMBER_H */
