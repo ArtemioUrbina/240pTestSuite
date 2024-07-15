3383a76a48e40a0f2637175ecf55fa6dd190bd60
Artemio <aurbina@junkerhq.net>
2024-07-15 14:52:26 -0600

    == N64 == - Menu structure update

--- 240psuite/N64/240pSuite.c
+++ 240psuite/N64/240pSuite.c
@@ -27,6 +27,9 @@
 
 void drawIntro(void);
 void drawPatternsMenu(void);
+void drawPatternsColorMenu(void);
+void drawPatternsGeometryMenu(void);
+void drawVideoTestsMenu(void);
  
 int main(void)
 {
@@ -53,7 +56,7 @@ int main(void)
 		
 		rdpqStart();
 		rdpqDrawImage(bg, 0, 0);
-		rdpqDrawImage(sd, 220, 80);
+		rdpqDrawImage(sd, 225, 75);
 		rdpqEnd();
 		
 		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "Test Patterns"); y += fh; c++;
@@ -87,6 +90,9 @@ int main(void)
 				case 1:
 					drawPatternsMenu();
 					break;
+				case 2:
+					drawVideoTestsMenu();
+					break;
 			}
 			reload = 1;
 		}
@@ -117,13 +123,13 @@ void drawPatternsMenu(void)
 		
 		rdpqStart();
 		rdpqDrawImage(bg, 0, 0);
-		rdpqDrawImage(sd, 220, 80);
+		rdpqDrawImage(sd, 225, 75);
 		rdpqEnd();
 		
 		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "Color & Black Levels"); y += fh; c++;
 		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "Geometry"); y += fh; c++;
-		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "Audio Tests"); y += fh; c++;
-		drawStringS(x, y + fh, r - 0x10, sel == c ? 0 : g, sel == c ? 0 : b, "Back to Main Menu"); y += fh;
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "HCFR Test Patterns"); y += fh; c++;
+		drawStringS(x, y + fh, r * 0.8, sel == c ? 0 : g, sel == c ? 0 : b, "Back to Main Menu"); y += fh;
 				
 		waitVsync();
 		
@@ -148,7 +154,229 @@ void drawPatternsMenu(void)
 			
 			switch(sel)
 			{
+				case 1:
+					drawPatternsColorMenu();
+					break;
+				case 2:
+					drawPatternsGeometryMenu();
+					break;
 				case 4:
+					exit = 1;
+					break;
+			}
+			reload = 1;
+		}
+		
+		if(keys.c[0].B)
+			exit = 1;
+	}
+	freeImage(&bg);
+	freeImage(&sd);
+}
+
+void drawPatternsColorMenu(void)
+{
+	int sel = 1, reload = 1, exit = 0;
+	struct controller_data keys;
+	sprite_t *bg = NULL, *sd = NULL;
+	
+	while(!exit) {
+		int c = 1, x = 45, y = 70;
+		int r = 0xFF, g = 0xFF, b = 0xFF;
+		
+		if(reload)
+		{
+			bg = sprite_load("rom:/mainbg.sprite");
+			sd = sprite_load("rom:/sd.sprite");
+			reload = 0;
+		}
+		
+		getDisplay();
+		
+		rdpqStart();
+		rdpqDrawImage(bg, 0, 0);
+		rdpqDrawImage(sd, 225, 75);
+		rdpqEnd();
+		
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "PLUGE"); y += fh; c++;
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "Color Bars"); y += fh; c++;
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "EBU Color Bars"); y += fh; c++;
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "SMPTE Color Bars"); y += fh; c++;
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "Color Bars/Gray"); y += fh; c++;
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "Color Bleed Check"); y += fh; c++;
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "Gray Ramp"); y += fh; c++;
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "White & RGB screens"); y += fh; c++;
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "100 IRE"); y += fh; c++;
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "Sharpness"); y += fh; c++;
+		drawStringS(x, y + fh, r * 0.8, sel == c ? 0 : g, sel == c ? 0 : b, "Back to Patterns Menu"); y += fh;
+				
+		waitVsync();
+		
+		controller_scan();
+        keys = Controller_ButtonsDown();
+
+		if(keys.c[0].up)
+			sel--;
+
+		if(keys.c[0].down)
+			sel++;
+			
+		if(sel > c)
+			sel = 1;
+		if(sel < 1)
+			sel = c;
+		
+		if(keys.c[0].A)
+		{	
+			freeImage(&bg);
+			freeImage(&sd);
+			
+			switch(sel)
+			{
+				case 11:
+					exit = 1;
+					break;
+			}
+			reload = 1;
+		}
+		
+		if(keys.c[0].B)
+			exit = 1;
+	}
+	freeImage(&bg);
+	freeImage(&sd);
+}
+
+void drawPatternsGeometryMenu(void)
+{
+	int sel = 1, reload = 1, exit = 0;
+	struct controller_data keys;
+	sprite_t *bg = NULL, *sd = NULL;
+	
+	while(!exit) {
+		int c = 1, x = 55, y = 90;
+		int r = 0xFF, g = 0xFF, b = 0xFF;
+		
+		if(reload)
+		{
+			bg = sprite_load("rom:/mainbg.sprite");
+			sd = sprite_load("rom:/sd.sprite");
+			reload = 0;
+		}
+		
+		getDisplay();
+		
+		rdpqStart();
+		rdpqDrawImage(bg, 0, 0);
+		rdpqDrawImage(sd, 225, 75);
+		rdpqEnd();
+		
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "Monoscope"); y += fh; c++;
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "Grid"); y += fh; c++;
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "Overscan"); y += fh; c++;
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "Convergence"); y += fh; c++;
+		drawStringS(x, y + fh, r * 0.8, sel == c ? 0 : g, sel == c ? 0 : b, "Back to Patterns Menu"); y += fh;
+				
+		waitVsync();
+		
+		controller_scan();
+        keys = Controller_ButtonsDown();
+
+		if(keys.c[0].up)
+			sel--;
+
+		if(keys.c[0].down)
+			sel++;
+			
+		if(sel > c)
+			sel = 1;
+		if(sel < 1)
+			sel = c;
+		
+		if(keys.c[0].A)
+		{	
+			freeImage(&bg);
+			freeImage(&sd);
+			
+			switch(sel)
+			{
+				case 5:
+					exit = 1;
+					break;
+			}
+			reload = 1;
+		}
+		
+		if(keys.c[0].B)
+			exit = 1;
+	}
+	freeImage(&bg);
+	freeImage(&sd);
+}
+
+void drawVideoTestsMenu(void)
+{
+	int sel = 1, reload = 1, exit = 0;
+	struct controller_data keys;
+	sprite_t *bg = NULL, *sd = NULL;
+	
+	while(!exit) {
+		int c = 1, x = 45, y = 55;
+		int r = 0xFF, g = 0xFF, b = 0xFF;
+		
+		if(reload)
+		{
+			bg = sprite_load("rom:/mainbg.sprite");
+			sd = sprite_load("rom:/sd.sprite");
+			reload = 0;
+		}
+		
+		getDisplay();
+		
+		rdpqStart();
+		rdpqDrawImage(bg, 0, 0);
+		rdpqDrawImage(sd, 225, 75);
+		rdpqEnd();
+		
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "Drop Shadow Test"); y += fh; c++;
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "Striped Sprite Test"); y += fh; c++;
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "Lag Test"); y += fh; c++;
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "Timing & Reflex Test"); y += fh; c++;
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "Scroll Test"); y += fh; c++;
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "Grid Scroll Test"); y += fh; c++;
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "Horz./Vert. Stripes"); y += fh; c++;
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "Checkerboard"); y += fh; c++;
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "Phase & Sample Rate"); y += fh; c++;
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "Disappearing Logo"); y += fh; c++;
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "Backlit Zone Test"); y += fh; c++;
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "Diagonal Test"); y += fh; c++;
+		drawStringS(x, y, r, sel == c ? 0 : g, sel == c ? 0 : b, "Alternating 240p/480i Test"); y += fh; c++;
+		drawStringS(x, y + fh/2, r * 0.8, sel == c ? 0 : g, sel == c ? 0 : b, "Back to Patterns Menu"); y += fh;
+				
+		waitVsync();
+		
+		controller_scan();
+        keys = Controller_ButtonsDown();
+
+		if(keys.c[0].up)
+			sel--;
+
+		if(keys.c[0].down)
+			sel++;
+			
+		if(sel > c)
+			sel = 1;
+		if(sel < 1)
+			sel = c;
+		
+		if(keys.c[0].A)
+		{	
+			freeImage(&bg);
+			freeImage(&sd);
+			
+			switch(sel)
+			{
+				case 14:
 					exit = 1;
 					break;
 			}
@@ -169,6 +397,8 @@ void drawIntro()
 	sprite_t *ld = NULL;
 	
 	ld = sprite_load("rom:/libdragon.sprite");
+	if(!ld)
+		return;
 	
 	while(delay) {
 		getDisplay();
@@ -183,7 +413,7 @@ void drawIntro()
         keys = Controller_ButtonsDown();
 		
 		delay --;
-		if(keys.c[0].A)
+		if(keys.c[0].A || keys.c[0].B)
 			delay = 0;
 	}
 	
